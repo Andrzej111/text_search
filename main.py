424b24aa6553ae48f1b4772e4605ecb1dac509a8
pawel szulc <pawel_szulc@onet.pl>
2016-07-28 00:19:01 +0200

    Another env stuff

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from StringMatcher import StringMatcher
-
+from SlidingBag import SlidingBag
 if __name__ == "__main__":
 
     ss = StringMatcher()
     ss.set_seqs('poprawiam','pozdrawiam')
 
     print (ss.distance())
+
+    sb = SlidingBag()
+    sb.set_doc('txt_files/t2')
+    sb.set_word()
+    sb.run()
